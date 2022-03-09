9af3ff6ef8c95ca926a2d0302a5c6e98d5b18645
Adrian-Luerssen <91116739+Adrian-Luerssen@users.noreply.github.com>
2022-03-09 18:00:50 +0100

    Update Task.java

--- app/src/main/java/com/example/tothelistapp/Task.java
+++ app/src/main/java/com/example/tothelistapp/Task.java
@@ -1,4 +1,39 @@
 package com.example.tothelistapp;
 
+import java.sql.Time;
+
 public class Task {
+    private String taskName;
+    private Time dueDate;
+    private boolean finished;
+
+    public Task(String taskName, Time dueDate, boolean finished) {
+        this.taskName = taskName;
+        this.dueDate = dueDate;
+        this.finished = finished;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public Time getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Time dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
 }
